dd5fa2761d263075328a6d61c393594bb9d10902
Paul Storey <paul.number12@googlemail.com>
2021-12-22 11:20:22 +0000

    Implementation for adding an item to the list.
    In addition to providing an implementation for adding items to the list,
    this commit also includes some JavaScript documentation using JSDoc and
    uses the 'defer' attribute in preference to jQuery methods for ensuring
    that todolist.js is executed once other page resources have been loaded.

--- todolist.js
+++ todolist.js
@@ -1,5 +1,8 @@
-$(document).ready(function() {
+"use strict";
+// Documentation is loosely based on JSDoc (https://jsdoc.app/)
+// Developed using Atom with the atom-termjs package.
 
+(function() {
   let exampleList = [{
       name: "trekking poles",
       notes: "Should be collapsible into 3 or 4 sections to make stowage inside your rucksack easier. Expect to pay approx £80",
@@ -27,17 +30,30 @@ $(document).ready(function() {
     },
   ];
 
+  /**
+   * Asynchronously calls the 'interested' function
+   * with 'value' as an argument, after a short delay.
+   */
   let valueAfterDelay = function(value, interested) {
     setTimeout(function() {
       interested(value);
     }, 500);
   };
 
+  /**
+   * Returns a string value which is composed using the specified
+   * input and is suitable for using as an 'id' value for a
+   * DOM element.
+   */
   let generateIdValueUsing = function(input) {
     const regex = /\W/ig;
     return "id" + input.replaceAll(regex, '');
   };
 
+  /**
+   * Create a child element of the specified type with the specified
+   * class names and attributes.
+   */
   let createChildOf = function(parent, elementType, classNames, attributes) {
     let newElement = document.createElement(elementType);
     if (classNames) {
@@ -54,11 +70,21 @@ $(document).ready(function() {
     return newElement;
   }
 
+  /**
+   * Creates a text node using the specified text
+   * and appends it to the specified parent.
+   */
   let appendTextNodeTo = function(parent, text) {
     parent.appendChild(document.createTextNode(text));
   };
 
-  let addListItem = function(listItem, listItems) {
+   /**
+   * Adds the specified list item to the DOM. The listItem
+   * object is expected to have the following properties: name
+   * (string), notes (string), urls (array of strings).
+   */
+  let addListItemToDOM = function(listItem) {
+    let listItems = document.getElementById("listItems");
     let isFirstItem = listItems.children.length == 0;
     let itemId = generateIdValueUsing(listItem.name);
     let card = createChildOf(listItems, "div", ["card", "border-info", "mb-2"]);
@@ -107,7 +133,7 @@ $(document).ready(function() {
     let form = createChildOf(modalBody, "form");
     let fieldset = createChildOf(form, "fieldset", ["form-group"]);
     let notesDiv = createChildOf(fieldset, "div", ["form-group"]);
-    let label = createChildOf(notesDiv, "label", undefined, {
+    let label = createChildOf(notesDiv, "label", ["form-control-label"], {
       "for": "edit-" + itemId + "-notes"
     });
     appendTextNodeTo(label, "notes");
@@ -137,21 +163,26 @@ $(document).ready(function() {
       }
     });
     $("#" + modal.getAttribute("id")).on('hidden.bs.modal', function(e) {
+      // Directly calling 'anchor.focus()' wasn't working, so...
       window.setTimeout(() => anchor.focus(), 0);
     });
   };
 
+  /**
+   * Expecting the list to be an array of objects each with a 'name', some 'notes' and some
+   * 'urls', this function rebuilds the DOM representation of this list.
+   */
   let layoutList = function(name, list) {
-    let listItems = document.querySelector("#listItems");
-    let listName = document.querySelector("#listName");
     let listHeaderChildren = document.querySelectorAll("#listHeader > *");
     Array.prototype.forEach.call(listHeaderChildren, function(listHeaderItem) {
       listHeaderItem.classList.remove("d-none");
     });
+    let listItems = document.getElementById("listItems");
+    let listName = document.getElementById("listName");
     listItems.replaceChildren();
     listName.replaceChildren(name);
     list.forEach(function(listItem) {
-      addListItem(listItem, listItems);
+      addListItemToDOM(listItem);
     });
   };
 
@@ -159,6 +190,11 @@ $(document).ready(function() {
     alert(error);
   };
 
+  /**
+   * Returns a Promise to deliver a 2-element array in which the first element
+   * is listName and the second is a list.
+   * The list is a list of objects. Each object has a name, some notes and some urls.
+   */
   let fetchList = function(listName) {
     return new Promise(function(succeed, fail) {
       valueAfterDelay([listName, exampleList], succeed);
@@ -196,4 +232,52 @@ $(document).ready(function() {
       showError(error);
     });
 
-});
+  let addListItemNameInputListener = function(event) {
+    // To rebuild this list every time is extremly inefficient.
+    // It would be better to add this event listener 'on the fly',
+    // with a constructed list of 'taken'.
+    // However this would require removal of the event listener when
+    // the dialog is closed/hidden.
+    // Not impossible!
+    let addListItemName = document.getElementById("addListItemName");
+    let addListItemButton = document.getElementById("addListItemButton");
+    let addListItemNameText = document.getElementById("addListItemNameText");
+    let taken = exampleList.map(listItem => generateIdValueUsing(listItem.name));
+    let candidateName = addListItemName.value.trim()
+    if ( candidateName ) {
+      if ( taken.indexOf(generateIdValueUsing(candidateName)) > -1 ) {
+        addListItemButton.classList.add("disabled");
+        addListItemNameText.textContent = "A list item with this name already exists";
+      }
+      else {
+        addListItemButton.classList.remove("disabled");
+        // https://stackoverflow.com/questions/19347988/make-empty-div-of-one-line-height/43102506
+        addListItemNameText.innerHTML = "&#8203;";
+      }
+    }
+    else { // candidateName is empty
+      addListItemButton.classList.add("disabled");
+      addListItemNameText.textContent = "Mandatory";
+    }
+  };
+
+  document.getElementById("addListItemName").addEventListener("input", addListItemNameInputListener );
+
+  document.getElementById("addListItemButton").addEventListener("click", function(event) {
+    let newListItem = {
+      "name":   document.getElementById("addListItemName").value,
+      "notes":  document.getElementById("addListItemNotes").value,
+      "urls": [ document.getElementById("addListItemUrl").value ]
+    };
+    exampleList.push(newListItem);
+    addListItemToDOM(newListItem);
+  });
+
+  $("#addListItemModalDialog").on('hidden.bs.modal', function(e) {
+    document.getElementById("addListItemName").value = "";
+    document.getElementById("addListItemNameText").textContent = "Mandatory";
+    document.getElementById("addListItemNotes").value = "";
+    document.getElementById("addListItemUrl").value = "";
+  });
+
+})();
